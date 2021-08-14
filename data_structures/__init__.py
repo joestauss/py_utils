c5e27ee036e520078c03785d1f5ec208006e71dd
Joe Stauss <jstauss008@gmail.com>
2021-08-14 09:07:32 -0700

    CSV Tuple List

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -2,3 +2,4 @@ from .json_dict    import JSONDict, ReadOnlyJSONDict
 from .base_classes import BaseCollection, BaseMapping, BaseSequence, BaseSet
 from .multi_dict   import MultiDict
 from .error_dict   import ErrorDict
+from .csv_tuple_list import CSVTupleList
